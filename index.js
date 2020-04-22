07c13b58d7bee61701b73216c9d89bcd9c90bacb
richard-li-2015 <64157452+richard-li-2015@users.noreply.github.com>
2020-04-23 02:25:11 +0800

    Fourth Upload

---
+++ index.js
@@ -0,0 +1,107 @@
+/**
+ * Created by Administrator on 2017/8/16.
+ */
+
+$(document).ready(function(){
+
+    $("#Header1").mouseenter(function(){
+        $("#Header1").addClass("Nav1h",50);
+    });
+
+    $("#Header1").mouseleave(function(){
+        $("#Header1").removeClass("Nav1h",50);
+    });
+
+    $("#Header2").mouseenter(function(){
+        $("#Header2").addClass("Nav1h",50);
+    });
+
+    $("#Header2").mouseleave(function(){
+        $("#Header2").removeClass("Nav1h",50);
+    });
+
+    $("#Header3").mouseenter(function(){
+        $("#Header3").addClass("Nav1h",50);
+    });
+
+    $("#Header3").mouseleave(function(){
+        $("#Header3").removeClass("Nav1h",50);
+    });
+
+    $("#Header4").mouseenter(function(){
+        $("#Header4").addClass("Nav1h",50);
+    });
+
+    $("#Header4").mouseleave(function(){
+        $("#Header4").removeClass("Nav1h",50);
+    });
+
+    $("#Header5").mouseenter(function(){
+        $("#Header5").addClass("Nav1h",50);
+    });
+
+    $("#Header5").mouseleave(function(){
+        $("#Header5").removeClass("Nav1h",50);
+    });
+
+    $("#Header6").mouseenter(function(){
+        $("#Header6").addClass("Nav1h",50);
+    });
+
+    $("#Header6").mouseleave(function(){
+        $("#Header6").removeClass("Nav1h",50);
+    });
+
+});
+
+$(document).ready(function(){
+
+    $("#Footer1").mouseenter(function(){
+        $("#Footer1").addClass("Nav3h",50);
+    });
+
+    $("#Footer1").mouseleave(function(){
+        $("#Footer1").removeClass("Nav3h",50);
+    });
+
+    $("#Footer2").mouseenter(function(){
+        $("#Footer2").addClass("Nav3h",50);
+    });
+
+    $("#Footer2").mouseleave(function(){
+        $("#Footer2").removeClass("Nav3h",50);
+    });
+
+    $("#Footer3").mouseenter(function(){
+        $("#Footer3").addClass("Nav3h",50);
+    });
+
+    $("#Footer3").mouseleave(function(){
+        $("#Footer3").removeClass("Nav3h",50);
+    });
+
+    $("#Footer4").mouseenter(function(){
+        $("#Footer4").addClass("Nav3h",50);
+    });
+
+    $("#Footer4").mouseleave(function(){
+        $("#Footer4").removeClass("Nav3h",50);
+    });
+
+    $("#Footer5").mouseenter(function(){
+        $("#Footer5").addClass("Nav3h",50);
+    });
+
+    $("#Footer5").mouseleave(function(){
+        $("#Footer5").removeClass("Nav3h",50);
+    });
+
+    $("#Footer6").mouseenter(function(){
+        $("#Footer6").addClass("Nav3h",50);
+    });
+
+    $("#Footer6").mouseleave(function(){
+        $("#Footer6").removeClass("Nav3h",50);
+    });
+
+});
